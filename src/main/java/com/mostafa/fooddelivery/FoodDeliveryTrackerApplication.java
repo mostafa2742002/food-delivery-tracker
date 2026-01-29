6c825e7340909b01c81a8fa018b802812cf983a7
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-01-29 13:38:42 +0200

    first commit

---
+++ src/main/java/com/mostafa/fooddelivery/FoodDeliveryTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mostafa.fooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mostafa/fooddelivery/FoodDeliveryTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mostafa.fooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
