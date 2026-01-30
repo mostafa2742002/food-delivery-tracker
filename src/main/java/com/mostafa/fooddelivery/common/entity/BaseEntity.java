04847ee2a9dac61dbd2c4ea2c83917bb78fffca8
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-01-30 21:15:55 +0200

    add: create BaseEntity class for common entity attributes

---
+++ src/main/java/com/mostafa/fooddelivery/common/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.mostafa.fooddelivery.common.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.*;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
