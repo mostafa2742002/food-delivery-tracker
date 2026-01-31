cb1919149c15779b35c81bd23041339bf681670b
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-01-31 14:24:30 +0200

    add: create Role enum and User entity with attributes

---
+++ src/main/java/com/mostafa/fooddelivery/user/entity/Role.java
@@ -0,0 +1,10 @@
+package com.mostafa.fooddelivery.user.entity;
+
+
+
+public enum Role {
+    CUSTOMER,
+    ADMIN,
+    RESTAURANT_OWNER,
+    DRIVER  
+}
---
+++ src/main/java/com/mostafa/fooddelivery/user/entity/User.java
@@ -0,0 +1,44 @@
+package com.mostafa.fooddelivery.user.entity;
+
+import com.mostafa.fooddelivery.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User extends BaseEntity{
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder.Default
+    @Column(name = "is_active")
+    private boolean isActive = true;
+}
